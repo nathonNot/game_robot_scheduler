dd088a7e68ccda56cc6addc4d4d95b23f02c2cf9
liuyu <674197141@qq.com>
2021-01-24 15:10:42 +0800

    空值校验

--- server/app/api/api_couponse.py
+++ server/app/api/api_couponse.py
@@ -9,7 +9,7 @@ import app.config as cfg
 router = APIRouter()
 
 router.get("generator")
-async def generator_couponse(gm_key:str,num:int):
+async def generator_couponse(gm_key:str="",num:int=1):
     if gm_key != cfg.get_config_dc()["couponse_key"]:
         return "密钥不正确"
     return md_couponse.generate_conponse(num)
