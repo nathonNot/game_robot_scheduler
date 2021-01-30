3895a4dc046c89b1ffee46cee0c94769f5d47c76
674197141@qq.com <Qq7925955>
2021-01-30 21:10:51 +0800

    form获取数据

--- server/app/api/api_hook.py
+++ server/app/api/api_hook.py
@@ -13,15 +13,11 @@ router = APIRouter()
 @router.post("/ali_pay")
 async def alipay_gatwey(request: Request):
     data = await request.form()
-    query_data = request.query_params
-    logger.info(query_data)
     order_id = data.get("out_trade_no","")
-    logger.info("body数据--------------------")
-    logger.info(request.body)
     if order_id == "":
         logger.error(data)
         return "error"
-    md_order.inpay_order(data)
+    await md_order.inpay_order(data)
     logger.info(data)
     return "success"
 
