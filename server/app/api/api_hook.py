ec619cc543291be491486e259c50edc1b036123f
liuyu <674197141@qq.com>
2021-01-30 14:28:50 +0800

    输出一下回调

---
+++ server/app/api/api_hook.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter
+from starlette.requests import Request
+from app.util.rsa import return_respons
+import json
+from loguru import logger
+
+router = APIRouter()
+
+
+@router.get("/ali_pay")
+async def alipay_hook(request: Request):
+    logger.info(request.params)
+    logger.info(request.body())
+    return "ok"
+
+@router.post("/ali_pay")
+async def alipay_gatwey(request: Request):
+    logger.info(request.params)
+    logger.info(request.body())
+    return "ok"
