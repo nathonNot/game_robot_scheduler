bf174fc444164733ded18471dc7d4d3941afc7dd
liuyu <674197141@qq.com>
2021-02-11 15:31:12 +0800

    以后不再跟踪js

--- server/html/js/buy.js
+++ server/html/js/buy.js
@@ -1,4 +1,18 @@
 
 function checkBtn(){
-    alert("11111");
+
+    var usePhone = document.getElementById("cbb_usePhone").checked;
+    var name = document.getElementById("user_name").value;
+    if (usePhone){
+        alert(name);
+    }
+}
+
+function on_use_phone_click(){
+    var usePhone = document.getElementById("cbb_usePhone").checked;
+    if (usePhone){
+        document.getElementById("user_name").placeholder = "请输入手机号";
+    }else{
+        document.getElementById("user_name").placeholder = "请输入账号名";
+    }
 }
